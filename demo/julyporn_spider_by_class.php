8280fc3c2a2a85c87788a6c8ae9dc51026fc7a0d
Nova <nodilaswii@gmail.com>
2020-06-12 23:10:38 +0800

    添加小说爬虫

--- demo/julyporn_spider_by_class.php
+++ demo/julyporn_spider_by_class.php
@@ -1,11 +1,12 @@
 <?php
 
 require "../core/webapp_connect.php";
+require "../library/cls_query.php";
+$conn = new webapp_connect('http://www.baidu.com');
 
 $requestQuery = parse_url($url);
 var_dump(pathinfo('https://julyporn.com/stories/all/free?page=1'));
+
+$requestQuery->request('GET', "/stories/all/free?page=1");
+
 exit;
-
-// $conn = new webapp_connect('https://julyporn.com/');
-
-// $conn->request(join('?', [$requestQuery['path']]));
--- demo/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-$b = $a = 5;
-var_dump($b);
-$a = $b[3];
-echo $a;
-error_get_last();
